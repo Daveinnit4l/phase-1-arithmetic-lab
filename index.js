873b15972e99ffb7dfe23330a22f5109403d56e9
kyle <davek2647@gmail.com>
2023-06-21 18:51:09 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 
-// Write your code here
+  {
+    let num1=31;
+    let num2=2;
+ let multiply=(num1*num2)
+ console.log(multiply);
+  }
+  {
+   
+ let random= Math.ceil(Math.random ()*100);
+ random;
+  }
+
+{
+    let num3=29;
+    let num4=5;
+    let mod =(num3%num4);
+    console.log (mod);
+}
+{
+let max=Math.max(12,13,14,15,16,17,18,19,20)
+console.log(max);
+}
+
